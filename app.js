41b6a92c6b080b10a05c33fcf10fe1ddda72034c
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-03-06 13:25:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"), (err) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    }
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"), (err) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    }
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"), (err) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"), (err) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    }
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  } else {
+    console.log(`My server is listening to the PORT n°${PORT}`);
+  }
+});
