3d45de61f003a23e6d7ec63184281f50de63dd35
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-03-11 12:39:12 +0100

    feat: add view templates with ejs and render them to the client

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ import path, { dirname } from "path";
 
 const __filepath = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filepath);
-
 const app = express();
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+// Configuring EJS and route for dynamic html
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { name: "Camulogène" });
+});
+
+// Serving Static Assets
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use("/about", profileRouter);
 app.use("/", indexRouter);
@@ -19,7 +39,6 @@ app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "404.html"));
 });
 
-// We recognize error middleware function as his first parameter being 'err', and as a four must-be present parameters.
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(err.statusCode || 500).send(err.message);
